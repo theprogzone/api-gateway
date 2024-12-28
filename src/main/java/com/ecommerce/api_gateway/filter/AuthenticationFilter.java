8b3ba21a9d191419b28406e57d984e2b3e8d830e
anuradha <anuradha.kadurugasyaya@gmail.com>
2024-12-28 14:06:27 +0800

    Added detailed comments

--- src/main/java/com/ecommerce/api_gateway/filter/AuthenticationFilter.java
+++ src/main/java/com/ecommerce/api_gateway/filter/AuthenticationFilter.java
@@ -25,20 +25,30 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
 
     List<String> urls = List.of("user/login", "user/register");
 
+    /**
+     * Filtering the requests coming from Front End
+     */
     @Override
     public GatewayFilter apply(Config config) {
 
         return (((exchange, chain) -> {
+            // Check the request need be authenticated or not
             if (isAuthenticate(exchange.getRequest().getURI().getPath())) {
+                // If the request need to be authenticated, first check whether request header contains the Authorization keyword or not
+                // If it is not contains, throw ValidationException
                 if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
                     throw new ValidationException("Missing authorization header in the request");
                 }
 
+                // Accessing the authorization header
+                // From Authorization header, we are extracting the token
                 String authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
                 if (authHeader != null && authHeader.startsWith("Bearer ")) {
                     authHeader = authHeader.substring(7);
                 }
 
+                // Extracted token we are passing to authentication service to validate the token
+                // If the token is invalid, api gateway returns bad request http status to the front end
                 webClientBuilder.build().get().uri("http://auth-service/auth/api/v1/user/validateToken?token=" + authHeader)
                         .exchangeToMono(response -> {
                             int statusCode = response.statusCode().value();
